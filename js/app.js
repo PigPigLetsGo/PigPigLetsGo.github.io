acf0cc47a09470243e079fc16ec93149f9915702
PigPigLetsGo <1851644015@qq.com>
2026-09-23 16:33:17 +0800

    Site updated: 2026-09-23 16:33:15

--- js/app.js
+++ js/app.js
@@ -2756,7 +2756,7 @@ const siteInit = function () {
 
 window.addEventListener('DOMContentLoaded', siteInit);
 
-console.log('%c Theme.Shoka v' + CONFIG.version + ' %c https://shoka.lostyu.me/ ', 'color: white; background: #e9546b; padding:5px 0;', 'padding:4px;border:1px solid #e9546b;')
+console.log('%c Theme.Shoka v' + CONFIG.version + ' %c 念念不忘，必有回响。 ', 'color: white; background: #e9546b; padding:5px 0;', 'padding:4px;border:1px solid #e9546b;')
 var canvasEl = document.createElement('canvas');
 canvasEl.style.cssText = 'position:fixed;top:0;left:0;pointer-events:none;z-index:9999999';
 document.body.appendChild(canvasEl);
